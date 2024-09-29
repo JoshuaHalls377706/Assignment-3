8e7a526145d11d39fe3f87af33a581ea83a73410
BStokes-HIT137 <s364762@students.cdu.edu.au>
2024-09-29 21:24:08 +0800

    Update authtoken.py

--- authtoken.py
+++ authtoken.py
@@ -1,4 +1,5 @@
 # authtoken.py
+#Have refreshed token need to review how to keep it safe
 
 # How to get one: https://huggingface.co/docs/hub/security-tokens
 auth_token = "{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}"
