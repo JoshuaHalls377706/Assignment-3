8c59a21a6fbba7731ab2225c8d9d3115095cbc62
BStokes-HIT137 <s364762@students.cdu.edu.au>
2024-09-30 10:37:46 +0800

    Update authtoken.py
    scrambled top secret code

--- authtoken.py
+++ authtoken.py
@@ -2,8 +2,5 @@
 #Have refreshed token need to review how to keep it safe
 
 # How to get one: https://huggingface.co/docs/hub/security-tokens
-<<<<<<< HEAD
+
 auth_token = "{hf_ paste over this with the top secret code jIaogmcNTdvjBZtEzwoquwCqnR)}"
-=======
-auth_token = "{hf_UNfQhWQhJfSAoarxRxYrjfYSxZTWZi}"
->>>>>>> faf3f88c128a9a9f3a39dd0f61eda3328fd24b17
